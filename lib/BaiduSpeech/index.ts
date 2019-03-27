cd0ac8a70341b389e408d45eb4fc20d3ff9d1ad5
hzgod <554332816@qq.com>
2019-03-27 13:31:39 +0800

    fix baidu语音问题

--- lib/BaiduSpeech/index.ts
+++ lib/BaiduSpeech/index.ts
@@ -1,5 +1,5 @@
-const speechText = (token: String, text: String, cuid = 'cuid'): void => {
-    let url = `https://tts.baidu.com/text2audio?lan=zh&ctp=1&cuid=${cuid}&vol=9&spd=3&tok=${token}&tex=${text}`
+const speechText = (token: String, text: String, cuid = 'cuid', vol = 10, spd = 3): void => {
+    let url = `https://tts.baidu.com/text2audio?lan=zh&ctp=1&cuid=${cuid}&vol=${vol}&spd=${spd}&tok=${token}&tex=${text}`
     let n = new Audio(url);
     n.src = url;
     n.play();
